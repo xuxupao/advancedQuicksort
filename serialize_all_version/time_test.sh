8df2f14187c369e3ae7e3055fdcbe17c9236978f
xuxupao <73543889+xuxupao@users.noreply.github.com>
2023-12-02 12:07:37 -0300

    Update time_test.sh

--- serialize_all_version/time_test.sh
+++ serialize_all_version/time_test.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-rm resultados.csv
+rm resultados_serial.csv
 # Compila a bagaça
 #make all
 
 # Arquivo CSV saida
-echo "Tamanho,Tempo,EXE,BOUND,TOTAL_STALLS,SERIAL_STALLS" > resultados.csv
+echo "Tamanho,Tempo,EXE,TOTAL_STALLS,SERIAL_STALLS,BAD_SPEC,FE_BOUND,BE_BOUND,BR_MIS" > resultados.csv
 
 for ((j=10; j<=100000; j=j*10))
 do
     for ((i=0; i<1000; i++))
     do
-        out=$(perf stat -e EXE_ACTIVITY.EXE_BOUND_0_PORTS,TOPDOWN.BACKEND_BOUND_SLOTS,CYCLE_ACTIVITY.STALLS_TOTAL,RESOURCE_STALLS.SCOREBOARD ./quicksort $j)
+        out=$(perf stat -e EXE_ACTIVITY.EXE_BOUND_0_PORTS,CYCLE_ACTIVITY.STALLS_TOTAL,RESOURCE_STALLS.SCOREBOARD ./quicksort $j)
         elapsed_time=$(echo "$out" | grep "Sorting" | awk '{print $5}')
-        prf_dm=$(echo "$out" | awk 'NR==3{print $1}')
-        br_msp=$(echo "$out" | awk 'NR==3{print $2}')
+        exe=$(echo "$out" | grep "EXE_ACTIVITY.EXE_BOUND_0_PORTS" | awk '{print $1}')
+        total_stalls=$(echo "$out" | grep "CYCLE_ACTIVITY.STALLS_TOTAL" | awk '{print $1}')
+        serial_stalls=$(echo "$out" | grep "RESOURCE_STALLS.SCOREBOARD" | awk '{print $1}')
+        
+        out=$(perf stat ./quicksort $j)
+        bad_spec=$(echo "$out" | grep "cpu_core/topdown-bad-spec/" | awk '{print $4}')
+        bad_spec=$(echo "$bad_spec" | sed 's/,/./; s/%//')
+        fe_bound=$(echo "$out" | grep "cpu_core/topdown-fe-bound/" | awk '{print $4}')
+        fe_bound=$(echo "$fe_bound" | sed 's/,/./; s/%//')
+        be_bound=$(echo "$out" | grep "cpu_core/topdown-be-bound/" | awk '{print $4}')
+        be_bound=$(echo "$be_bound" | sed 's/,/./; s/%//')
+        br_mis=$(echo "$out" | grep "cpu_core/topdown-br-mispredict/" | awk '{print $4}')
+        br_mis=$(echo "$br_mis" | sed 's/,/./; s/%//')
 
-        echo "$j,$elapsed_time,$prf_dm,$br_msp" >> resultados.csv
+        echo "$j,$elapsed_time,$exe,$total_stalls,$serial_stalls,$bad_spec,$fe_bound,$be_bound,$br_mis" >> resultados_serial.csv
     done
 done
 
