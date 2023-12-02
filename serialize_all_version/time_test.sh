e2d8771d60dd3ca54d0288e60cddf1a1a1a3710f
xuxupao <73543889+xuxupao@users.noreply.github.com>
2023-12-02 01:48:09 -0300

    Update time_test.sh

--- serialize_all_version/time_test.sh
+++ serialize_all_version/time_test.sh
@@ -5,13 +5,13 @@ rm resultados.csv
 #make all
 
 # Arquivo CSV saida
-echo "Tamanho,Tempo,PRF_DM,BR_MSP" > resultados.csv
+echo "Tamanho,Tempo,EXE,BOUND,TOTAL_STALLS,SERIAL_STALLS" > resultados.csv
 
 for ((j=10; j<=100000; j=j*10))
 do
     for ((i=0; i<1000; i++))
     do
-        out=$(./quicksort $j)
+        out=$(perf stat -e EXE_ACTIVITY.EXE_BOUND_0_PORTS,TOPDOWN.BACKEND_BOUND_SLOTS,CYCLE_ACTIVITY.STALLS_TOTAL,RESOURCE_STALLS.SCOREBOARD ./quicksort $j)
         elapsed_time=$(echo "$out" | grep "Sorting" | awk '{print $5}')
         prf_dm=$(echo "$out" | awk 'NR==3{print $1}')
         br_msp=$(echo "$out" | awk 'NR==3{print $2}')
